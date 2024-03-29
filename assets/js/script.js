671afd9a26d9d31c51a8fda4e8e50a112719d953
LuisLopez-developer <luislopezhuari27@gmail.com>
2024-03-29 15:31:18 -0500

    se añade que el img se cambie de acuerdo al tema

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 var themeimg = document.getElementById("themeImg");
 
 themeimg.onclick = function () {
-    document.body.classList.toggle("dark-theme")
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        themeimg.src = "assets/img/sun.png"
+    } else {
+        themeimg.src = "assets/img/moon.png"
+    }
 }
